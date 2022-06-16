5d51fea259ae56946845eedbd44530359bf176ee
Josie Lindley <josephine.lucyrita@gmail>
2022-06-16 15:46:02 +0100

    complete task 3

---
+++ getPets.js
@@ -0,0 +1,35 @@
+const https = require('https');
+const fs = require('fs');
+
+
+
+fs.readFile('northcoders.json', 'utf-8', (err, people)=>{
+    const employees = JSON.parse(people);
+    const petOwners = [];
+    let count = 0
+    employees.forEach((employee)=>{
+        const requestObject = {
+            hostname: 'nc-leaks.herokuapp.com',
+            path: `/api/people/${employee.username}/pets`
+        };
+        const req = https.request(requestObject, (response) =>{
+            if(response.statusCode === 200){
+                let packets = ''
+                response.on('data', (packet)=>{
+                    packets += packet.toString();
+                })
+                response.on('end', ()=>{
+                    const result = JSON.parse(packets);
+                    petOwners.push(result);
+                    count++
+                    if(petOwners.length === count);{
+                        const petOwnersString = JSON.stringify(petOwners);
+                        fs.writeFile('pets.json', petOwnersString, ()=>{});
+                    }
+                    
+                });
+            }
+        })
+        req.end();
+    })
+})
